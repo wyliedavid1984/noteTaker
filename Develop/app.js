314f9620ba94d1b0da98d4996a5f8d2be79103b4
wylie <wyliedavid1984@gmail.com>
2021-01-03 12:47:32 -0500

    all pages displaying

--- Develop/app.js
+++ Develop/app.js
@@ -2,13 +2,13 @@ const express = require("express")
 
 // express app
 const app = express();
+// port or set port to 3000
+const PORT = process.env.PORT || 3000;
 
-//listen for requests
-app.listen(3000);
 
 app.get("/", (req, res) =>{
-    
-    res.status(200).sendFile("./views/index.html", { root: __dirname })
+   
+   res.status(200).sendFile("./views/index.html", { root: __dirname })    
 })
 
 app.get("/notes", (req, res) => {
@@ -20,3 +20,6 @@ app.use((req, res)=> {
 
     res.status(404).sendFile("./views/404.html", { root: __dirname })
 })
+
+//listen for requests
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
