ee8dfd9fb55f3ff197a3c780f8888a5977b86709
wylie <wyliedavid1984@gmail.com>
2021-01-03 13:06:30 -0500

    add path module and created path names variables

--- Develop/app.js
+++ Develop/app.js
@@ -1,24 +1,29 @@
-const express = require("express")
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 // express app
 const app = express();
-// port or set port to 3000
+// port from Heroku or set port to 3000
 const PORT = process.env.PORT || 3000;
+// create paths
+const index = path.join(__dirname, '/views/index.html');
+const notes = path.join(__dirname, './views/notes.html');
+const page404 = path.join(__dirname, './views/404.html') 
 
-
-app.get("/", (req, res) =>{
+app.get('/', (req, res) =>{
    
-   res.status(200).sendFile("./views/index.html", { root: __dirname })    
+   res.status(200).sendFile(index)    
 })
 
-app.get("/notes", (req, res) => {
+app.get('/notes', (req, res) => {
 
-    res.status(200).sendFile("./views/notes.html", { root: __dirname })
+    res.status(200).sendFile(notes)
 })
 
 app.use((req, res)=> {
 
-    res.status(404).sendFile("./views/404.html", { root: __dirname })
+    res.status(404).sendFile(page404)
 })
 
 //listen for requests
