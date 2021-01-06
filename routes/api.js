df4df45e54d35c9f035c16c2d44ad23770626eae
wylie <wyliedavid1984@gmail.com>
2021-01-06 15:47:16 -0500

    added promise to delete

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,12 @@
 // built in modules
+const {
+    rejects
+} = require('assert');
 const express = require('express')
-const fs = require('fs')
+const fs = require('fs');
+const {
+    resolve
+} = require('path');
 const path = require('path')
 const util = require('util')
 // created modules
@@ -42,7 +48,7 @@ router.post('/api/notes', (req, res) => {
     addId(db)
     //writing new file to db.json with new user input
     writeFileAsync(dbPath, JSON.stringify(db), (err) => {
-        err ? console.log(err) : console.log("write success")       
+        err ? console.log(err) : console.log("write success")
     })
     // ending response
     res.json(db)
@@ -55,23 +61,26 @@ router.delete("/api/notes/:id", (req, res) => {
     // set variable to id of selected object.
     const id = parseInt(req.params.id);
     // filtering out selected id and return new array
-    const newNotes = db.filter((note) => note.id !== id)
-    // writing new array to db.json file
-    console.log(newNotes)
-    let newDB = writeFileAsync(dbPath, JSON.stringify(newNotes)).then((data) => {
-        
-        console.log("delete success")
-        
-        console.log("promise", newDB)
-    console.log("end of delete request")
-
-    res.json(db)
-    
-    }).catch((err) => {
-        console.error(err)
+    let p = new Promise((resolve, reject) => {
+        const newNotes = db.filter((note) => note.id !== id)
+        if (newNotes.length > 1) {
+            resolve(newNotes)
+        } else {
+            reject('Failed')
+        }
     })
+    p.then((newNotes) => {
+        // writing new array to db.json file
+        writeFileAsync(dbPath, JSON.stringify(newNotes))
+            .then(() => {
+                console.log("end of delete request")
+            }).catch((err) => {
+                console.error(err)
+            })
+    })
+
     // ending response
-    
+    res.json(db)
     res.end()
 })
 
